51edb08598240cabb3839628394f9bf2c091f397
yvonne11yuting <yvonne11yuting@gmail.com>
2023-06-30 12:09:45 +0800

    feat: integrate google oauth

--- src/components/AuthProviders.tsx
+++ src/components/AuthProviders.tsx
@@ -1,6 +1,43 @@
-import React from 'react'
+"use client"; // not can be render on the server
+
+import { getProviders, signIn } from 'next-auth/react'
+import { useState, useEffect } from 'react';
+
+interface Provider {
+    id: string;
+    name: string;
+    type: string;
+    signinUrl: string;
+    callbackUrl: string;
+    signinUrlParams?: Record<string, string> | null
+}
+
+type Providers = Record<string, Provider>;
 
 const AuthProviders = () => {
+    const [providers, setProviders] = useState<Providers | null>(null);
+
+    useEffect(() => {
+        const fetchProviders = async () => {
+            const res = await getProviders();
+
+            console.log(res);
+
+            setProviders(res);
+        }
+        fetchProviders();
+    }, [])
+
+    if (providers) {
+        return (
+            <div>{Object.values(providers).map((provider: Provider, i) => (
+                <button key={i} onClick={() => signIn(provider?.id)}>
+                    {provider.id}
+                </button>
+            ))}</div>
+        )
+    }
+
     return (
         <div>AuthProviders</div>
     )
