29104b654e19303fba1cd7c5f485dbf54fdefccb
yvonne11yuting <yvonne11yuting@gmail.com>
2023-07-03 13:31:17 +0800

    feat: add projectCard

--- graphql/index.ts
+++ graphql/index.ts
@@ -43,3 +43,33 @@ export const createProjectMutation = `
         }
     }
 `;
+
+export const projectsQuery = `
+  query getProjects($category: String, $endcursor: String) {
+    projectSearch(first: 8, after: $endcursor, filter: {category: {eq: $category}}) {
+      pageInfo {
+        hasNextPage
+        hasPreviousPage
+        startCursor
+        endCursor
+      }
+      edges {
+        node {
+          title
+          githubUrl
+          description
+          liveSiteUrl
+          id
+          image
+          category
+          createdBy {
+            id
+            email
+            name
+            avatarUrl
+          }
+        }
+      }
+    }
+  }
+`;
--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -1,5 +1,5 @@
 import { GraphQLClient } from 'graphql-request'
-import { createProjectMutation, createUserMutation, getUserQuery } from '../../graphql';
+import { createProjectMutation, createUserMutation, getUserQuery, projectsQuery } from '../../graphql';
 import { ProjectForm } from '@/common.types';
 
 const isProduction = process.env.NODE_ENV === 'production';
@@ -76,4 +76,10 @@ export const createNewProject = async (form: ProjectForm, creatorId: string, tok
         }
         return makeGraphQLRequest(createProjectMutation, variables)
     }
+}
+
+export const fetchAllProjects = async (category?: string, endcursor?: string) => {
+    client.setHeader('x-api-key', apiKey);
+
+    return makeGraphQLRequest(projectsQuery, { category, endcursor })
 }
