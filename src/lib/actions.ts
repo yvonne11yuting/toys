7d7687a0362ef2e254499fedfc5838e0f1ea290d
yvonne11yuting <yvonne11yuting@gmail.com>
2023-07-03 17:56:54 +0800

    fix incorrect key

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -3,8 +3,8 @@ import { createProjectMutation, createUserMutation, getProjectByIdQuery, getProj
 import { ProjectForm } from '@/common.types';
 
 const isProduction = process.env.NODE_ENV === 'production';
-const apiUrl = isProduction ? process.env.NEXT_PUBLIC_GARFBASE_API_URL || '' : 'http://127.0.0.1:4000/graphql'
-const apiKey = isProduction ? process.env.NEXT_PUBLIC_GARFBASE_API_KEY || '' : 'letmein';
+const apiUrl = isProduction ? process.env.NEXT_PUBLIC_GRAFBASE_API_URL || '' : 'http://127.0.0.1:4000/graphql'
+const apiKey = isProduction ? process.env.NEXT_PUBLIC_GRAFBASE_API_KEY || '' : 'letmein';
 const serverUrl = isProduction ? process.env.NEXT_PUBLIC_SERVER_URL : 'http://localhost:3000';
 
 const client = new GraphQLClient(apiUrl)
