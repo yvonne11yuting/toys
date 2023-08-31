280d4621f058c3350a7b61032a866684c859ccc4
YuTing <42829087+yvonne11yuting@users.noreply.github.com>
2023-09-01 02:02:52 +0800

    feat: init welcome and books page (#3)
    * fix error
    
    * remove unused files
    
    * remove unused files
    
    * fix: use html entity

---
+++ src/app/books/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Books = () => {
+    return (
+        <div className="pt-8">
+            <div>Thank you to my friend Judith for hosting a book club. Because of this, I&apos;ve read many books.</div>
+        </div>
+    )
+}
+
+export default Books
