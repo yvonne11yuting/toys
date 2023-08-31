78d24f7c06e453360504b53accd3357ceb82262d
yvonne11yuting <yvonne11yuting@gmail.com>
2023-09-01 00:56:19 +0800

    fix error

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -9,7 +9,7 @@ type Props = {
 }
 
 const UserProfile = async ({ params }: Props) => {
-    const result = await getUserProjects(params?.id || '', 100) as { user: UserProfile }
+    const result = { user: null };
 
     if (!result?.user) return (
         <p className="no-result-text">Failed to fetch user info</p>
