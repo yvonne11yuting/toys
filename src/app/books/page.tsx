cb74a2a4db6548a53bd13a14dc2b3d4b46e89739
yvonne11yuting <yvonne11yuting@gmail.com>
2023-09-01 02:01:30 +0800

    fix: use html entity

--- src/app/books/page.tsx
+++ src/app/books/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Books = () => {
     return (
         <div className="pt-8">
-            <div>Thank you to my friend Judith for hosting a book club. Because of this, I've read many books.</div>
+            <div>Thank you to my friend Judith for hosting a book club. Because of this, I&apos;ve read many books.</div>
         </div>
     )
 }
