34fb0b9dbed0a6846e91a50b7a5c2119f40a64c3
yvonne11yuting <yvonne11yuting@gmail.com>
2023-09-05 14:31:10 +0800

    feat: modify wording

--- src/app/books/page.tsx
+++ src/app/books/page.tsx
@@ -13,8 +13,8 @@ const Books = () => {
                 <Volume2 color="#ffbb00" />
                 <AlertTitle className="ml-2 p-1 text-stone-800">Join us!! 大家一起來唸書~</AlertTitle>
                 <AlertDescription className="ml-2 mt-2 leading-relaxed text-stone-600">
-                    「三日不讀書，面目可憎。」其實也沒這麼兇啦，大家來快樂唸書吧！<br />
-                    地點會在雙北，歡迎有興趣的朋友一起分享書吧。<br />
+                    「三日不讀書，面目可憎。」其實也沒這麼兇，大家一起來讀點書吧~<br />
+                    地點會在雙北，歡迎有興趣的朋友一起分享各種書。<br />
                     書的種類不限，想看什麼就看什麼！<br />
                     有興趣的小夥伴可以聯繫我喔！<br />
                     <b className="text-amber-800">Email: <a target="_blank" href="mailto:yvonne11yuting@gmail.com" rel="noreferrer noopener">yvonne11yuting@gmail.com</a></b>
